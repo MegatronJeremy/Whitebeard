8b78d29131af1d6887a553c618d4feb14b5043d5
Fica01 <52139923+Fica01@users.noreply.github.com>
2022-09-16 02:51:05 +0200

    Fixed some errors, added testbench

---
+++ VGAController/vga_test.vhd
@@ -0,0 +1,28 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+entity vga_test_bench is
+end entity vga_test_bench;
+
+architecture test of vga_test_bench is
+	signal mr : std_logic := '1';
+	signal vclk : std_logic := '0';
+	
+	constant Tvclk : time := 20 ns;
+begin
+	
+	clk_gen: vclk <= not vclk after Tvclk/2;
+	
+	dut: entity work.VGAcontroller port map (
+		vclk, mr
+	);
+	
+	stimulus: process
+	begin	
+		mr <= '0';
+		wait for 2*Tvclk;
+		mr <= '1';
+		wait for 10*Tvclk;
+		
+	end process;
+end test;
