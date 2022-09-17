51bc8798ff17bb1c28342c76733a46ee21b6bce3
Fica01 <52139923+Fica01@users.noreply.github.com>
2022-09-18 01:58:07 +0200

    Trying to fix signal loss

--- VGAController/vga_test.vhd
+++ VGAController/vga_test.vhd
@@ -8,21 +8,26 @@ architecture test of vga_test_bench is
 	signal mr : std_logic := '1';
 	signal vclk : std_logic := '0';
 	
+	signal enbl : std_logic := '0';
+	
 	constant Tvclk : time := 20 ns;
 begin
 	
 	clk_gen: vclk <= not vclk after Tvclk/2;
 	
 	dut: entity work.VGAcontroller port map (
-		vclk, mr
+		vclk, mr, enbl
 	);
 	
 	stimulus: process
-	begin	
+	begin
+		wait for 2 * Tvclk;
+		enbl <= '1';
+		wait for 2 * Tvclk;
 		mr <= '0';
-		wait for 2*Tvclk;
+		wait for 4 * Tvclk;
 		mr <= '1';
-		wait for 10*Tvclk;
-		
+		wait for Tvclk;
+		wait;
 	end process;
 end test;
